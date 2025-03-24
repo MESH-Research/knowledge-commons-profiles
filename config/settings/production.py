c41ef3d0629e900de55c3a4242836a165d45915d
Martin Paul Eve <martin@eve.gd>
2025-03-24 12:48:22 +0000

    build(django): allow all hosts as alb is dynamic

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,13 +16,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [
-    "hcommons.org",
-    "localhost",
-    "profile.hcommons.org",
-    "kcprofiles-app",
-    "django",
-]
+ALLOWED_HOSTS = ["*"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
