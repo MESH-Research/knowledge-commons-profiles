c3d513ade668c9abed4e97032986ebd1dc13d82e
Martin Paul Eve <martin@eve.gd>
2025-04-02 17:02:41 +0100

    fix(csrf): change csrf settings so test pass

--- config/settings/local.py
+++ config/settings/local.py
@@ -93,10 +93,7 @@ INSTALLED_APPS += ["django_extensions", "sslserver"]
 
 # SECURITY
 # ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
-SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
 CSRF_COOKIE_SECURE = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-name
